390878c7e921c5eeb9ada28061f4944193f95c12
Darren Black <dblack@totaltrash.net>
2022-03-09 14:10:58 +1100

    Ensure phx-remove is fired

--- lib/my_app_web/live/home_live.ex
+++ lib/my_app_web/live/home_live.ex
@@ -18,19 +18,24 @@ defmodule MyAppWeb.HomeLive do
       <div id="has_hook" phx-hook="MyHook"></div>
 
       <!-- `MyHook.destroyed` fires -->
-      <div id="has_hook_and_phx_remove" phx-remove={some_js()} phx-hook="MyHook"></div>
+      <div id="has_hook_and_phx_remove" phx-remove={fire_js("#js_fired_1")} phx-hook="MyHook"></div>
 
       <!-- `MyHook.destroyed` does not fire -->
-      <div phx-remove={some_js()}>
+      <div phx-remove={fire_js("#js_fired_2")}>
         <div id="has_hook_and_parent_has_phx_remove" phx-hook="MyHook"></div>
       </div>
 
       <%= live_patch "Hide", to: Routes.home_path(@socket, :index) %>
     <% end %>
+
+    <h2 style="margin-top: 2em">Indicate when phx-remove is fired:</h2>
+    <span id="js_fired_1">js_fired_1 fired</span><br />
+    <span id="js_fired_2">js_fired_2 fired</span>
     """
   end
 
-  defp some_js(js \\ %JS{}) do
+  defp fire_js(js \\ %JS{}, indicator) do
     js
+    |> JS.transition("fire", to: indicator, time: 500)
   end
 end
